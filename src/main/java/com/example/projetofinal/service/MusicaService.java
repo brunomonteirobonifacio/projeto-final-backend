3a51bbb7095d4501595f55587759e38cdbc42001
Bruno Monteiro Bonifacio <bruno.343923@alunosatc.edu.br>
2025-11-06 21:34:16 -0300

    Adicionado método de update ao MusicaController e criado DTO auxiliar MusicaUpdateDTO

--- src/main/java/com/example/projetofinal/service/MusicaService.java
+++ src/main/java/com/example/projetofinal/service/MusicaService.java
@@ -2,6 +2,7 @@ package com.example.projetofinal.service;
 
 import com.example.projetofinal.dto.MusicaCreateDTO;
 import com.example.projetofinal.dto.MusicaDTO;
+import com.example.projetofinal.dto.MusicaUpdateDTO;
 import com.example.projetofinal.model.Musica;
 import com.example.projetofinal.repository.MusicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,19 +30,19 @@ public class MusicaService {
 
     public MusicaDTO findById(UUID id) {
         return musicaRepository.findById(id)
-                .map(this::entityToDTO)
+                .map(this::toDTO)
 //                .orElse(() -> new RuntimeException("Música não encontrada"));
                 .orElse(null);
     }
 
     public MusicaDTO create(MusicaCreateDTO musicaDTO) {
-        Musica musica = dtoToEntity(musicaDTO);
+        Musica musica = toEntity(musicaDTO);
         musica = musicaRepository.save(musica);
 
-        return entityToDTO(musica);
+        return toDTO(musica);
     }
 
-    private MusicaDTO entityToDTO(Musica musica) {
+    private MusicaDTO toDTO(Musica musica) {
         MusicaDTO dto = new MusicaDTO();
         dto.setId(musica.getId());
         dto.setNome(musica.getNome());
@@ -50,12 +51,37 @@ public class MusicaService {
         return dto;
     }
 
-    private Musica dtoToEntity(MusicaCreateDTO dto) {
+    public MusicaDTO update(UUID id, MusicaUpdateDTO dto) {
+        Musica musica = musicaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Música não encontrada"));
+
+        musica.setDuracaoEmSegundos(dto.getDuracaoEmSegundos());
+
+//        TODO: descomentar quando artistaRepository e generoRepository estiverem criados
+//        if (!musica.getGenero().getId().equals(dto.getGeneroId())) {
+//            Genero genero = generoRepository.findById(dto.getGeneroId())
+//                    .orElseThrow(() -> new RuntimeException("Gênero não encontrado"));
+
+//            musica.setGenero(genero);
+//        }
+
+//        if (!musica.getArtista().getId().equals(dto.getArtistaId())) {
+//            Artista artista = artistaRepository.findById(dto.getArtistaId())
+//                    .orElseThrow(() -> new RuntimeException("Artista não encontrado"));
+//
+//            musica.setArtista(artista);
+//        }
+
+        Musica musicaUpdated = musicaRepository.save(musica);
+        return toDTO(musicaUpdated);
+    }
+
+    private Musica toEntity(MusicaCreateDTO dto) {
         Musica musica = new Musica();
         musica.setNome(dto.getNome());
         musica.setDuracaoEmSegundos(dto.getDuracaoEmSegundos());
 
-//        TODO: descomentar quando essas partes ficarem prontas
+//        TODO: descomentar quando artistaRepository e generoRepository estiverem criados
 //        Artista artista = artistaRepository.findById(dto.getArtistaId())
 //                .orElseThrow(() -> new RuntimeException("Artista não encontrado"));
 
