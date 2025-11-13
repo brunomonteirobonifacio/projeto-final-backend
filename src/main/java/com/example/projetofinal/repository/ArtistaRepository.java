6993dec8f8925b52cc4052b12377f2583b5c3ae5
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-11-13 20:03:51 -0300

    Alterados ArtistaRepository e GeneroRepository para estenderem JpaRepository

--- src/main/java/com/example/projetofinal/repository/ArtistaRepository.java
+++ src/main/java/com/example/projetofinal/repository/ArtistaRepository.java
@@ -1,4 +1,9 @@
 package com.example.projetofinal.repository;
 
-public interface ArtistaRepository {
+import com.example.projetofinal.model.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ArtistaRepository extends JpaRepository<Artista, UUID> {
 }
--- src/main/java/com/example/projetofinal/repository/GeneroRepository.java
+++ src/main/java/com/example/projetofinal/repository/GeneroRepository.java
@@ -1,4 +1,9 @@
 package com.example.projetofinal.repository;
 
-public interface GeneroRepository {
+import com.example.projetofinal.model.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GeneroRepository extends JpaRepository<Genero, UUID> {
 }
