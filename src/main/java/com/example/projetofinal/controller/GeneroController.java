b8acd6061f268bdbcc4a658a82cd47bcf591a262
unknown <mrlslv11@gmail.com>
2025-11-13 22:12:11 -0300

    Adicionado código de GeneroController

--- src/main/java/com/example/projetofinal/controller/GeneroController.java
+++ src/main/java/com/example/projetofinal/controller/GeneroController.java
@@ -1,4 +1,64 @@
 package com.example.projetofinal.controller;
 
+import com.example.projetofinal.dto.GeneroDTO;
+import com.example.projetofinal.service.GeneroService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/genero")
 public class GeneroController {
+
+    @Autowired
+    private GeneroService generoService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Page<GeneroDTO> list(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "nome") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction,
+            @RequestParam(required = false) String nomeFiltro
+    ) {
+        Sort sort = direction.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return generoService.list(nomeFiltro, pageable);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public GeneroDTO findById(@PathVariable UUID id) {
+        return generoService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GeneroDTO save(@RequestBody @Valid GeneroDTO dto) {
+        return generoService.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public GeneroDTO update(@PathVariable UUID id, @RequestBody @Valid GeneroDTO dto) {
+        return generoService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id) {
+        generoService.delete(id);
+    }
 }
